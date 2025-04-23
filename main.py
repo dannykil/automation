1a4911bed9566f013cbe0807d942b789e47ccf4d
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2025-04-23 21:41:54 +0900

    updating main.py

--- main.py
+++ main.py
@@ -19,4 +19,5 @@ def main():
     return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
